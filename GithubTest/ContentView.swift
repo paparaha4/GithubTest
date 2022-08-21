255768f808fab3aec82dfb9f9810bdefe46efec7
paparaha4 <61214392+paparaha4@users.noreply.github.com>
2022-08-21 14:18:05 +0700

    Initial Commit

---
+++ GithubTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubTest
+//
+//  Created by 🆂🅸🅽🆇🆄🅻🅵🅸 𝔔𝔞𝔯 on 21/8/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by 🆂🅸🅽🆇🆄🅻🅵🅸 𝔔𝔞𝔯 on 21/8/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
